d7b395b7fbe6c71916b3374cc5e601e6ea3f4f5f
Joyantichy <120155482+Joyantichy@users.noreply.github.com>
2023-02-28 09:08:10 +0600

    first commit

---
+++ es-recap.js
@@ -0,0 +1,43 @@
+/*
+must know
+1.var let const
+2.default parameter
+3.template string
+    3.1:create multi line string(``)
+    3.2: add dynamic variable (expression):`${}`
+4.arrow Function
+    4.1:single parameter arrow function
+    4.2:two parameter arrow fucntion
+    4.3: arrow function with function body . and don't forget you have  to return explilcitly
+    44: no parametr arrow function
+5.map ,filter find
+6.optionla chaining
+
+*/
+
+// Good To have
+/*
+    1.spread oparator:[...numbers,12,243]
+    2.desturing 
+    2-1: object destructring .how to get propoerty  name as variable
+            const {x} = {x:3, y:5}
+
+        2-2: array destruing      
+        const [a,b] = [3,4,6,98] 
+    3.Default parameter
+    4.for each
+    5.class,object
+    6.dot notation vs bracket notation
+    
+
+
+*/
+// Should know the name
+/* reduce
+ * inheritaence
+ * prototypicla chain
+ * 
+ * 
+ * 
+ * 
+ */
